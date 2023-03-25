86744bf2465a817e4c524334b47fb91c84863d06
MyPC <1043358774@qq.com>
2023-03-25 18:49:24 +0800

    ex

--- main.go
+++ main.go
@@ -9,9 +9,8 @@ import (
 )
 
 var (
-	filename = "filelist.txt"
-	fromDir      = "Z:\\阿里云盘Open\\测试\\世界顶级畅销书530本"
-	toDir = "./filelist.xlsx"
+	fromDir = "Z:\\阿里云盘Open\\测试\\世界顶级畅销书530本"
+	toDir   = "./filelist.xlsx"
 )
 
 func main() {
@@ -59,39 +58,6 @@ func toExl() {
 
 	// 将文件保存到本地
 	err = f.SaveAs(toDir)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println("Done!")
-}
-
-func toTxt() {
-	// 打开要写入的文件
-	f, err := os.Create(filename)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer f.Close()
-
-	// 遍历目录下的文件
-	err = filepath.Walk(fromDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		// 如果当前文件是文件夹，则跳过
-		if info.IsDir() {
-			return nil
-		}
-		// 将文件名写入文件
-		_, err = fmt.Fprintf(f, "%s\n", info.Name())
-		if err != nil {
-			return err
-		}
-		return nil
-	})
 	if err != nil {
 		fmt.Println(err)
 		return
